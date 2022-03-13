acd4da9fe97e2ff603c75c7d1114ceb7d796fe3c
Kevin Salvador Casas <kevinsal.casas@gmail.com>
2022-03-12 20:36:29 -0600

    Almost done!

--- src/components/LocationInfo.js
+++ src/components/LocationInfo.js
@@ -6,7 +6,8 @@ const LocationInfo = ({location}) => {
     return(
     <div className="locationInfo">
         <h1>{location.name}</h1>
-        <div className="locationInfo--subtext">
+        <div className="locationInfo--subtext">        
+        <p><b>Number:</b> {location.id}</p>
           <p><b>Type: </b>{location.type}</p>
           <p><b>Dimension: </b>{location.dimension}</p>
           <p><b>Number of residents: </b>{location.residents === undefined ? 0 : location.residents.length}</p>
--- src/components/ResidentInfo.js
+++ src/components/ResidentInfo.js
@@ -12,12 +12,13 @@ const ResidentInfo = ({characterUrl}) =>{
 
     return(
         <div className="card">        
-            <h3>{characterObject.name}</h3>
-            <img src={characterObject.image} alt="Character" />      
-            <p>Status: <b>{characterObject.status}</b></p>
-            <p>Origin: <b>{characterObject.origin?.name}</b></p>
-            <p>Aparitions: <b>{characterObject.episode?.length}</b></p>
-
+            <img src={characterObject.image} alt="Character" />
+            <div className="card--text">
+                <h3>{characterObject.name}</h3>
+                <p>Status: <b>{characterObject.status}</b></p>
+                <p>Origin: <b>{characterObject.origin?.name}</b></p>
+                <p>Aparitions: <b>{characterObject.episode?.length}</b></p>
+            </div>
         </div>
     )
 }
